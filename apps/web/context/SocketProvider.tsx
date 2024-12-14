0cfeaa70447f3bde02ecdbd513e56dcb6f65784d
syedjamal12 <mdhasnat067@gmail.com>
2024-12-14 23:37:32 +0530

    connect socket with server and client

---
+++ apps/web/context/SocketProvider.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { Interface } from "node:readline/promises"
+import React, { useCallback, useEffect } from "react"
+import { io } from "socket.io-client"
+
+interface SocketProviderProps{
+    children?: React.ReactNode;
+}
+interface ISocketContext {
+    sendMessage : (msg : string)=>any
+} 
+
+const SocketContext = React.createContext<ISocketContext | null>(null)
+
+export const SocketProvider:React.FC<SocketProviderProps>=({children})=>{
+    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) =>{
+     console.log("send msg",msg)
+    },[])
+    
+useEffect(()=>{
+    const _socket = io('http://localhost:8000')
+    return()=>{
+    _socket.disconnect();
+    }
+},[])
+
+    return(
+        <SocketContext.Provider value={{sendMessage}}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
