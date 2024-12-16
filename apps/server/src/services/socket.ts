9b946831313e978ba62ea79b0a06cfa58406858f
syedjamal12 <mdhasnat067@gmail.com>
2024-12-16 14:39:02 +0530

    now msgs going server to redis and redis to client

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,5 +1,19 @@
 // import { Socket } from "dgram";
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub = new Redis({
+    host:"caching-39129b63-mdhasnat294-d59c.f.aivencloud.com",
+    port:14574,
+    username:"default",
+    password:"AVNS_OMHL3khZJo1hfRr4v1B"
+})
+const sub = new Redis({
+    host:"caching-39129b63-mdhasnat294-d59c.f.aivencloud.com",
+    port:14574,
+    username:"default",
+    password:"AVNS_OMHL3khZJo1hfRr4v1B"
+})
 
 class SocketService {
     public initListners(){
@@ -8,7 +22,15 @@ class SocketService {
             console.log("new socket conneted",socket.id)
           socket.on("event:message", async({message}:{message:string})=>{
             console.log("New Message Recieved",message)
+            //send msg in redis
+            await pub.publish("MESSAGES", JSON.stringify({message}));
           })
+        })
+        sub.on("message",(channel, message)=>{
+            if(channel=="MESSAGES")
+            {
+                this.io.emit("message",message);
+            }
         })
     }
 
@@ -22,6 +44,7 @@ class SocketService {
                 origin: "*"
             }
         });
+        sub.subscribe("MESSAGES")
     }
 
     get io(){
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,18 +4,21 @@ import { useState } from "react";
 import { useSocket } from "../context/SocketProvider"
 
 export default function Page(){
-const {sendMessage} = useSocket();
+const {sendMessage,msgs} = useSocket();
 const[message,setMessage]=useState('')
 
   return(
     <div>
-      <div>
-        <h1>All Messages Appear here</h1>
-      </div>
+      
       <div>
         <input placeholder="message" onChange={(e) => setMessage(e.target.value)}></input>
         <button onClick={(e) => sendMessage(message)}>send</button>
       </div>
+      {
+        msgs.map((e)=>(
+          <li>{e}</li>
+        ))
+      }
     </div>
   )
 }
