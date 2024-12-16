9fe6d0c0c5bc568d723b2ce67a5596f05797da33
syedjamal12 <mdhasnat067@gmail.com>
2024-12-16 14:53:43 +0530

    redis to client

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,12 +1,19 @@
 // import { Socket } from "dgram";
 import { Server } from "socket.io";
 import Redis from "ioredis";
-
+import dotenv from "dotenv";
+dotenv.config();
 const pub = new Redis({
-    
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || "6379"),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
 })
 const sub = new Redis({
-   
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || "6379"),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
 })
 
 class SocketService {
